65379e979dd678778c9def06097593c3e6f422c2
k.budzik <k.budzik@praktyki.ideo.pl>
2020-10-04 09:55:34 +0200

    Implemented Repository and UoW

---
+++ Uplift/Uplift.DataAccess/Data/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category= new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
