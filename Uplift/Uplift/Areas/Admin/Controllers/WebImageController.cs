03207d0e8ced876990620f9c6258b2e854ace935
k.budzik <k.budzik@praktyki.ideo.pl>
2020-10-06 12:31:06 +0200

    Implemented photo uploader

---
+++ Uplift/Uplift/Areas/Admin/Controllers/WebImageController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+using Uplift.Utility;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class WebImageController : Controller
+    {
+        //NO REPOSITORY PATTERN
+        private readonly ApplicationDbContext _db;
+
+        public WebImageController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+
+            WebImages imageObj = new WebImages();
+            if (id == null)
+            {
+                //create
+            }
+            else
+            {
+                //update
+                imageObj = _db.WebImages.SingleOrDefault(m => m.Id == id);
+                if (imageObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return View(imageObj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(int id, WebImages imageObj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] uploadedPicture = null;
+                    using (var fileStream = files[0].OpenReadStream())
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            fileStream.CopyTo(memoryStream);
+                            uploadedPicture = memoryStream.ToArray();
+                        }
+                    }
+                    imageObj.Picture = uploadedPicture;
+                }
+                //jeśli id==0 -> DODAWANIE
+                if (imageObj.Id == 0)
+                {
+                    _db.WebImages.Add(imageObj);
+                }
+                else
+                {
+                    var imageFromDb = _db.WebImages.Where(c => c.Id == id).FirstOrDefault();
+                    imageFromDb.Name = imageObj.Name;
+                    if (files.Count > 0)
+                    {
+                        imageFromDb.Picture = imageObj.Picture;
+                    }
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imageObj);
+        }
+
+
+        #region APICALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //    return Json(new {data = _unitOfWork.Category.GetAll()});
+            //using stored procedure:
+            return Json(new { data = _db.WebImages.ToList()});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _db.WebImages.Find(id);
+            if (objFromDb == null)
+            {
+                return Json(new {success = false, message = "Error while deleting."});
+            }
+            _db.WebImages.Remove(objFromDb);
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Deleted successfully" });
+        }
+        #endregion
+    }
+}
---
+++ Uplift/Uplift/wwwroot/js/webImage.js
@@ -0,0 +1,69 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url": "/admin/webImage/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            //właściwe kolumny z jsona
+            { "data": "name", "width": "50%" },
+            {
+                // renderowanie przycisków
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                        <a href="/Admin/webImage/Upsert/${data}" class="btn btn-success text-white" style= "cursor:pointer; width:100px">
+                            <i class="far fa-edit"></i> Edit
+                        </a>
+
+                        <a class="btn btn-danger text-white" style= "cursor:pointer; width:100px" onclick=Delete("/Admin/webImage/Delete/${data}")>
+                            <i class="far fa-trash-alt"></i> Delete
+                        </a>
+                        </div>
+                    `;
+
+                },
+                "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted you will not be able to recover.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: "delete",
+                    url: url,
+                    success: function (data) {   //data argument is returned by API as JSON
+                        if (data.success) {
+                            toastr.success(data.message);
+                            dataTable.ajax.reload();
+                        } else {
+                            toastr.error(data.message);
+                        }
+                    }
+                });
+            }
+        });
+}
